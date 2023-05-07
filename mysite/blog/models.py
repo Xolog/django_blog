8d70c31c929e8dbffe39703d218ff81ff60ec995
Павел <stalk200213@gmail.com>
2023-05-07 23:36:34 +0300

    Доделана функция для каноничных урлов дружественных поисковым системам

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -43,4 +43,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.id])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
