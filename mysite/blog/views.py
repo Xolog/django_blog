ac4ecf89481b9fc0d241e92c08c1fdd1bc7e0625
Павел <stalk200213@gmail.com>
2023-05-10 00:51:02 +0300

    Реализована постраничная разбивка

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+    posts_list = Post.published.all()
+
+    # пагинация
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -18,6 +29,4 @@ def post_detail(request, year, month, day, post):
                              publish__day=day,
                              status=Post.Status.PUBLISHED)
 
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
