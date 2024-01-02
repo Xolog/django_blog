7b810ee508dfb151385c2c113363e72c7fc69aa5
Павел <stalk200213@gmail.com>
2024-01-02 17:08:34 +0300

    Создана система рекомендации постов по почте

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
 
 from .models import Post
@@ -38,17 +38,23 @@ def post_detail(request, year, month, day, post):
 
 
 def post_share(request, post_id):
+    # извлечь пост по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
     if request.method == 'POST':
         # Форма была передана на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-            # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cd["name"]} recommend you read {post.title}'
+            message = f'Read {post.title} at {post_url}\n\n {cd["name"]}\'s comments: {cd["comments"]}'
+            send_mail(subject, message, 'stalk200213@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'form': form,
-                                                    'post': get_object_or_404(Post,
-                                                                              id=post_id,
-                                                                              status=Post.Status.PUBLISHED)})
+    return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
